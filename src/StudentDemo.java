e33924cfddc2b3fe38b021c391329a737b8e0a6c
hanat <hanatruneh23@gmail.com>
2026-03-26 23:01:55 +0200

    commit

--- src/StudentDemo.java
+++ src/StudentDemo.java
@@ -31,65 +31,85 @@ class MathCourse extends Course {
         System.out.println("Level: " + level);
     }
 }
-    class ProgramingCourse extends Course {
 
-        private String language;
+class ProgramingCourse extends Course {
 
-        public ProgramingCourse(String courseName, int numberOfHours, String language) {
-            super(courseName, numberOfHours);
-            this.language = language;
-        }
+    private String language;
 
-        @Override
-        void courseInfo() {
-            System.out.println("Course Name: " + courseName);
-            System.out.println("Number of Hours: " + numberOfHours);
-            System.out.println("Programming Language: " + language);
-        }
+    public ProgramingCourse(String courseName, int numberOfHours, String language) {
+        super(courseName, numberOfHours);
+        this.language = language;
     }
 
-        interface Student {
+    @Override
+    void courseInfo() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Number of Hours: " + numberOfHours);
+        System.out.println("Programming Language: " + language);
+    }
+}
 
-            String getName();
-            void addCourse(Course course);
+interface Student {
 
-        }
+    String getName();
 
-        class UniversityStudent implements Student {
+    void addCourse(Course course);
 
-            private String name;
-            List<Course> courses;
+}
 
-            public UniversityStudent(String name) {
-                this.name = name;
+class UniversityStudent implements Student {
+
+    private String name;
+    List<Course> courses;
+
+    public UniversityStudent(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void addCourse(Course course) {
+        System.out.println(name + " has added the course: " + course.courseName);
+    }
+}
+
+
+public class StudentDemo {
+
+    public static void main(String[] args) {
+        MathCourse mathCourse = new MathCourse("Calculus", 40, "Advanced");
+        mathCourse.courseInfo();
+
+        ProgramingCourse programingCourse = new ProgramingCourse("Java Programming", 60, "Java");
+        programingCourse.courseInfo();
+
+        UniversityStudent student = new UniversityStudent("Alice");
+        student.addCourse(mathCourse);
+        student.addCourse(programingCourse);
+
+
+        UniversityStudent student2 = new UniversityStudent("Bob");
+        student2.addCourse(programingCourse);
+
+        List<UniversityStudent> students = new ArrayList<>();
+        students.add(student);
+        students.add(student2);
+
+
+        List<Course> courses = new ArrayList<>();
+        courses.add(mathCourse);
+        courses.add(programingCourse);
+
+        for (UniversityStudent s : students) {
+            System.out.println("Student: " + s.getName());
+            for (Course c : courses) {
+                s.addCourse(c);
             }
-
-            @Override
-            public String getName() {
-                return name;
-            }
-
-            @Override
-            public void addCourse(Course course) {
-                System.out.println(name + " has added the course: " + course.courseName);
-            }
-        }
-
-
-
-    public class StudentDemo {
-
-        public static void main(String[] args) {
-            MathCourse mathCourse = new MathCourse("Calculus", 40, "Advanced");
-            mathCourse.courseInfo();
-
-            ProgramingCourse programingCourse = new ProgramingCourse("Java Programming", 60, "Java");
-            programingCourse.courseInfo();
-
-            List<Course> courses = new ArrayList<>();
-            courses.add(mathCourse);
-            courses.add(programingCourse);
-
         }
     }
+}
 

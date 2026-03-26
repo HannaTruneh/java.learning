ad462950bae092e6a555df3f2fb50d96cb74b53c
hanat <hanatruneh23@gmail.com>
2026-03-26 22:44:03 +0200

    commit

---
+++ src/StudentDemo.java
@@ -0,0 +1,95 @@
+import java.util.List;
+import java.util.ArrayList;
+
+abstract class Course {
+
+    String courseName;
+    int numberOfHours;
+
+    public Course(String courseName, int numberOfHours) {
+        this.courseName = courseName;
+        this.numberOfHours = numberOfHours;
+    }
+
+    abstract void courseInfo();
+
+}
+
+class MathCourse extends Course {
+
+    private String level;
+
+    public MathCourse(String courseName, int numberOfHours, String level) {
+        super(courseName, numberOfHours);
+        this.level = level;
+    }
+
+    @Override
+    void courseInfo() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Number of Hours: " + numberOfHours);
+        System.out.println("Level: " + level);
+    }
+}
+    class ProgramingCourse extends Course {
+
+        private String language;
+
+        public ProgramingCourse(String courseName, int numberOfHours, String language) {
+            super(courseName, numberOfHours);
+            this.language = language;
+        }
+
+        @Override
+        void courseInfo() {
+            System.out.println("Course Name: " + courseName);
+            System.out.println("Number of Hours: " + numberOfHours);
+            System.out.println("Programming Language: " + language);
+        }
+    }
+
+        interface Student {
+
+            String getName();
+            void addCourse(Course course);
+
+        }
+
+        class UniversityStudent implements Student {
+
+            private String name;
+            List<Course> courses;
+
+            public UniversityStudent(String name) {
+                this.name = name;
+            }
+
+            @Override
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public void addCourse(Course course) {
+                System.out.println(name + " has added the course: " + course.courseName);
+            }
+        }
+
+
+
+    public class StudentDemo {
+
+        public static void main(String[] args) {
+            MathCourse mathCourse = new MathCourse("Calculus", 40, "Advanced");
+            mathCourse.courseInfo();
+
+            ProgramingCourse programingCourse = new ProgramingCourse("Java Programming", 60, "Java");
+            programingCourse.courseInfo();
+
+            List<Course> courses = new ArrayList<>();
+            courses.add(mathCourse);
+            courses.add(programingCourse);
+
+        }
+    }
+
